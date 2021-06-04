e99c03c9f61d06cb625152c843908a5670849b05
Haikalsium <mhfgeek@gmail.com>
2021-06-05 03:19:13 +0800

    Train
    add train

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -19,3 +19,36 @@ window.addEventListener('scroll',(e)=>{
           nav.classList.remove("add-shadow");
         }
       });
+
+function showTimeline() {
+  var showTml = document.getElementById('timeline');
+  if (showTml.style.display === "none") {
+    showTml.style.display = "block";
+  } else {
+    showTml.style.display = "none";
+  }
+}
+
+function showRoute() {
+  var show = document.getElementById('timeline-detail')
+  var hide = document.getElementById('detail-price-train')
+
+  if(show.style.display === "none") {
+    show.style.display = "block";
+    hide.style.display = "none";
+  } else {
+    show.style.display = "none;"
+  }
+}
+
+function showPrice() {
+  var show = document.getElementById('timeline-detail')
+  var hide = document.getElementById('detail-price-train')
+
+  if(hide.style.display === "none") {
+    hide.style.display = "block";
+    show.style.display = "none";
+  } else {
+    hide.style.display = "none;"
+  }
+}
